0a44fac6befa5ea6c5317a5c314e8ee44044727e
Kevin <kevin.espinosa@uruit.com>
2023-04-19 01:00:33 -0500

    add movement

--- src/game_objects/player.js
+++ src/game_objects/player.js
@@ -1,30 +1,31 @@
+import { GameObjects } from 'phaser';
 import Game from '../scenes/game';
 
-class Player {
-    static SIZE = 32;
-    static SPEED = 3;
+class Player extends Phaser.GameObjects.Rectangle {
+  static SIZE = 32;
+  static SPEED = 3;
 
-    constructor() {
-        this.position = new Phaser.Math.Vector2(100, 100);
-        this.velocity = new Phaser.Math.Vector2(1, 0).scale(Player.SPEED);
+  constructor(scene, x, y) {
+    super(scene, x, y, Player.SIZE, Player.SIZE, 0xffffff);
+    this.rect = scene.add.rectangle(x, y, Player.SIZE, Player.SIZE, 0xffffff);
 
+    this.cursors = scene.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.rect.x -= Player.SPEED;
     }
-
-    update() {
-        this.position = this.position.add(new Phaser.Math.Vector2(this.velocity.x, this.velocity.y));
-        // invert velocity if we hit the edge of the screen
-        if (this.position.x < 0 || this.position.x > Game.WIDTH - Player.SIZE) {
-            this.velocity.x *= -1;
-        }
+    if (this.cursors.right.isDown) {
+      this.rect.x += Player.SPEED;
     }
-
-    draw(graphics) {
-        graphics.lineStyle(5, 0xFF00FF, 1.0);
-        graphics.fillStyle(0xFFFFFF, 1.0);
-        graphics.fillRect(this.position.x, this.position.y, Player.SIZE, Player.SIZE);
-        graphics.strokeRect(this.position.x, this.position.y, Player.SIZE, Player.SIZE);
+    if (this.cursors.up.isDown) {
+      this.rect.y -= Player.SPEED;
     }
-
+    if (this.cursors.down.isDown) {
+      this.rect.y += Player.SPEED;
+    }
+  }
 }
 
 export default Player;
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,32 +1,25 @@
 import Phaser from 'phaser';
 import Player from '../game_objects/player';
 
-class Game extends Phaser.Scene {  
+class Game extends Phaser.Scene {
   static WIDTH = 800;
   static HEIGHT = 600;
 
   constructor() {
-    super("Box Fury");
+    super('Box Fury');
   }
 
-  preload() {
-  }
+  preload() {}
 
   create() {
-    this.graphics = this.add.graphics(
-      { lineStyle: { width: 1, color: 0xffffff }, fillStyle: { color: 0xffffff } }
-    )
-    this.player = new Player();
+    this.graphics = this.add.graphics({
+      lineStyle: { width: 1, color: 0xffffff },
+      fillStyle: { color: 0xffffff },
+    });
+    this.player = new Player(this, 100, 100);
   }
   update() {
     this.player.update();
-
-    // Drawing
-
-    this.graphics.clear();
-
-    this.player.draw(this.graphics)
-
   }
 }
 
